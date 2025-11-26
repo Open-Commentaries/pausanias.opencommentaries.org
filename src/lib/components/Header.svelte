86cfebb26c70d7943a5bbc0c07435bfa4f499e14
Charles Pletcher <cpletcher2@gmail.com>
2025-11-25 19:17:59 -0500

    Fix a few other unresolved URLs

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,10 +1,14 @@
+<script lang="ts">
+  import { resolve } from "$app/paths";
+</script>
+
 <div class="navbar bg-base-100 shadow-sm">
   <div class="flex-1">
-    <a class="btn btn-ghost text-xl" href="/">A Pausanias Reader in Progress</a>
+    <a class="btn btn-ghost text-xl" href={resolve("/")}>A Pausanias Reader in Progress</a>
   </div>
   <div class="flex-none">
     <ul class="menu menu-horizontal px-1">
-      <li><a href="/about">About</a></li>
+      <li><a href={resolve("/about")}>About</a></li>
     </ul>
   </div>
 </div>
