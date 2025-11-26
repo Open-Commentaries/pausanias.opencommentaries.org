84e13b045b9c68f7923c538c0bcf481e7f77c7f9
Charles Pletcher <cpletcher2@gmail.com>
2025-11-25 19:02:03 -0500

    Add deployment action

--- src/routes/passages/[urn]/+page.server.ts
+++ src/routes/passages/[urn]/+page.server.ts
@@ -1,5 +1,29 @@
+import type { EntryGenerator, PageServerLoad } from './$types';
+import type { PassageConfig } from '$lib/types';
+
 import { getNamedEntitiesForPassage, getPassage } from '$lib/functions';
-import type { PageServerLoad } from './$types';
+
+function getPassageURNs(toc: PassageConfig[]): Array<{ urn: string }> {
+	if (!toc) {
+		return [];
+	}
+
+	return toc.flatMap((passage) => {
+		let subpassages: Array<{ urn: string }> = [];
+
+		if (passage?.subpassages) {
+			subpassages = getPassageURNs(passage.subpassages);
+		}
+
+		return [{ urn: passage.urn }, ...subpassages];
+	});
+}
+
+export const entries: EntryGenerator = async () => {
+	const { toc } = await getPassage('urn:cts:greekLit:tlg0525.tlg001:1.1');
+
+	return getPassageURNs(toc);
+};
 
 export const load: PageServerLoad = async ({ params: params }) => {
 	const { criticalText, comments, toc } = await getPassage(params.urn);
@@ -12,3 +36,5 @@ export const load: PageServerLoad = async ({ params: params }) => {
 		toc
 	};
 };
+
+export const prerender = true;
--- svelte.config.js
+++ svelte.config.js
@@ -5,10 +5,15 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter({
+			precompress: true
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	},
 	extensions: ['.svelte', '.svx']
 };
 
